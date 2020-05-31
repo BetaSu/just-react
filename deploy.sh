71773ddce0c70a4f42aa9027d4c7aa2ed88b4095
superorangecc@163.com <313439271@qq.com>
2020-05-31 20:41:37 +0800

    feat: config

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:BetaSu/just-react.git master:gh-pages
+
+cd -
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  title: 'React 技术揭秘',
+  description: 'React源码解析',
+  base: '/just-react/',
+  dest: 'dist'
+}
