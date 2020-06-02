e8cf62ae420a2495eedbda2b3b0dd075a8dd2624
superorangecc@163.com <313439271@qq.com>
2020-06-02 13:11:07 +0800

    feat: add deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,28 @@ module.exports = {
   title: 'React 技术揭秘',
   description: 'React源码解析',
   base: '/just-react/',
-  dest: 'dist'
+  dest: 'dist',
+  serviceWorker: false,
+  themeConfig: {
+    repo: 'BetaSu/just-react',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    nav: [
+      {
+        text: '配套代码',
+        link: 'https://github.com/BetaSu/react-on-the-way'
+      }
+    ],
+    sidebar: [
+      {
+        title: '设计思想',
+        collapsable: false,
+        children: [
+          
+        ]
+      }
+    ]
+  }
 }
