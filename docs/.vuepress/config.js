c7a3fd72a24ca8c7980dd6fe9f6031b61a88d7a2
superorangecc@163.com <313439271@qq.com>
2020-06-04 08:41:48 +0800

    feat: init first chart

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,16 @@ module.exports = {
   title: 'React 技术揭秘',
   description: 'React源码解析',
   base: '/just-react/',
-  dest: 'dist'
+  dest: 'dist',
+  themeConfig: {
+    sidebar: [
+      ['/', '前言'],
+      {
+        title: '第一章 准备工作',
+        children: [
+          ['/准备工作/React理念', 'React理念']
+        ]
+      }
+    ]
+  }
 }
