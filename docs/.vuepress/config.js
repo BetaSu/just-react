5daa54b77dabc5e07990d37e422ca23ab6f27571
superorangecc@163.com <313439271@qq.com>
2020-06-17 22:41:24 +0800

    fix: rm base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'React 技术揭秘',
   description: 'React源码解析',
-  base: '/just-react/',
+  // base: '/just-react/',
   dest: 'dist',
   serviceWorker: false,
   configureWebpack: {
